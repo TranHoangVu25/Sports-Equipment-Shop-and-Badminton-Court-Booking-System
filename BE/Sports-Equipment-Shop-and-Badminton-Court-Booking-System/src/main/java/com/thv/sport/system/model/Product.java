6ace7c364828aba06294c4d8301d48f092ef2e62
Tran_Hoang_Vu <hoangvudx2004@gmail.com>
2026-03-22 15:36:47 +0700

    feat(search): add api sync data and advance search

--- BE/Sports-Equipment-Shop-and-Badminton-Court-Booking-System/src/main/java/com/thv/sport/system/model/Product.java
+++ BE/Sports-Equipment-Shop-and-Badminton-Court-Booking-System/src/main/java/com/thv/sport/system/model/Product.java
@@ -57,5 +57,9 @@ public class Product {
     @OneToMany(mappedBy = "product", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JsonManagedReference
     private List<ProductImage> productImages;
+
+    @OneToMany(mappedBy = "product", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<ProductVariant> productVariants;
 }
 
