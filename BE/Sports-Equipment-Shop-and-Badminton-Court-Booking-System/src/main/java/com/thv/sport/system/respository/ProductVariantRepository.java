9c66a8d72c7cc76326849a94fd99a23710aebd06
Tran_Hoang_Vu <hoangvudx2004@gmail.com>
2026-03-30 10:57:45 +0700

    fix(checkout): fix decrease quantity

--- BE/Sports-Equipment-Shop-and-Badminton-Court-Booking-System/src/main/java/com/thv/sport/system/respository/ProductVariantRepository.java
+++ BE/Sports-Equipment-Shop-and-Badminton-Court-Booking-System/src/main/java/com/thv/sport/system/respository/ProductVariantRepository.java
@@ -1,11 +1,18 @@
 package com.thv.sport.system.respository;
 
+import com.thv.sport.system.model.Product;
 import com.thv.sport.system.model.ProductVariant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductVariantRepository extends JpaRepository<ProductVariant, Long> {
 
+    @Query("select pv from ProductVariant pv where pv.sku in :ids")
+    List<ProductVariant> findBySku(@Param("ids") List<String> skus);
 }
 
