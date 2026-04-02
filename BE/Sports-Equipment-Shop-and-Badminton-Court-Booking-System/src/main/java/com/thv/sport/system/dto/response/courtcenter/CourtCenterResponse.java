0d5aa859257b5aab3927c8ac45b041c6924337e5
Tran_Hoang_Vu <hoangvudx2004@gmail.com>
2026-04-03 00:10:01 +0700

    feat(court): add api search

---
+++ BE/Sports-Equipment-Shop-and-Badminton-Court-Booking-System/src/main/java/com/thv/sport/system/dto/response/courtcenter/CourtCenterResponse.java
@@ -0,0 +1,25 @@
+package com.thv.sport.system.dto.response.courtcenter;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourtCenterResponse {
+    private Long courtCenterId;
+    private String name;
+    private String locationDetail;
+    private String phoneNumber;
+    private Integer deleted;
+    private String imgUrl;
+    private LocalDateTime createdAt;
+
+}
--- BE/Sports-Equipment-Shop-and-Badminton-Court-Booking-System/src/main/java/com/thv/sport/system/respository/CourtCenterImageRepository.java
+++ BE/Sports-Equipment-Shop-and-Badminton-Court-Booking-System/src/main/java/com/thv/sport/system/respository/CourtCenterImageRepository.java
@@ -2,11 +2,16 @@ package com.thv.sport.system.respository;
 
 import com.thv.sport.system.model.CourtCenterImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CourtCenterImageRepository extends JpaRepository<CourtCenterImage, Long> {
-
+    @Query("select cci from CourtCenterImage cci where cci.courtCenter.courtCenterId in :ids")
+    List<CourtCenterImage> findListImgByCourCenterId(@Param("ids") List<Long> id);
 }
 
 
